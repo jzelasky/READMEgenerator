0d5b60fae34a20d0d3db3512b1f451286f3eaba5
Jessica Zelasky <jzelasky@gmail.com>
2023-01-27 10:47:43 -0500

    added questions

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// TODO: Include packages needed for this application
+// TODO: Create a function to write README file
+//function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+//function init() {}
+
+// Function call to initialize app
+//init();
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of your project?', 'What was your motivation for your project?', 'Why did you build this project?', 'What problem does your project solve?', 'What did you learn from your project?', 'What are the steps required to install your project?', 'What are the usage instructions for your project?', 'What licenses did your project use?', 'What are the Github user names of the contributors of your project?', 'What are the tests for your project?', 'What is your email address?']
+const {title, motivation, whyBuild, problem, learn, install, instructions, licenses, github, tests, email} = questions
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
