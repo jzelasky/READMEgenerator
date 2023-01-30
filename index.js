e639520270f8c041607ada8b09016afc61cb6942
Jessica Zelasky <jzelasky@gmail.com>
2023-01-30 10:40:02 -0500

    added license badge feature

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
-
-
 const inquirer = require('inquirer');
 const fs = require('fs');
+let badge
 
 function init () {
     inquirer
@@ -32,20 +29,50 @@ function init () {
             {   type: 'checkbox',
                 message: 'What licenses did your project use?',
                 name: 'licenses',
-                choices: ['license 1', 'license 2', 'license 3']},
+                choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'Mozilla Public License 2.0', 'The Unlicense', 'Other']},
             {   type: 'input',
-                message: 'What are the Github user names of the contributors of your project?',
-                name: 'github'},
+                message: 'If anyone other than yourself worked on this project please list their github usernames.',
+                name: 'contributing'},
             {   type: 'input',
                 message:'What are the tests for your project?',
                 name: 'tests'},
             {   type: 'input',
                 message: 'What is your email address?',
                 name: 'email'},
+            {   type: 'input',
+                message: 'What is your github username?',
+                name: "github"}
         ])
         .then((answers) => {
+            if (String(answers.licenses) === 'Apache License 2.0'){
+                 badge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+            } else if (String(answers.licenses) === 'GNU General Public License v3.0'){
+                badge = '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)'
+            } else if (String(answers.licenses) === 'MIT License'){
+                badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+            } else if (String(answers.licenses) === 'BSD 2-Clause "Simplified" License'){
+                badge = '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)'
+            } else if (String(answers.licenses) === 'BSD 3-Clause "New" or "Revised" License'){
+                badge = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+            } else if (String(answers.licenses) === 'Boost Software License 1.0'){
+                badge = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+            } else if (String(answers.licenses) === 'Creative Commons Zero v1.0 Universal'){
+                badge = '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)'
+            } else if (String(answers.licenses) === 'Eclipse Public License 2.0'){
+                badge = '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
+            } else if (String(answers.licenses) === 'Mozilla Public License 2.0'){
+                badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+            } else if (String(answers.licenses) === 'The Unlicense'){
+                badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+            } else if (String(answers.licenses) === 'Other'){
+                badge = ''
+            } 
+    
             fs.writeFile('README.md',
-            `# ${answers.title}
+            `
+# ${answers.title}
+
+${badge}
             
 ## Description
 
@@ -55,7 +82,7 @@ ${answers.motivation} ${answers.whyBuild} ${answers.problem} ${answers.learn}
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
+- [Contributing](#contributing)
 - [License](#license)
 
 ## Installation
@@ -66,19 +93,26 @@ ${answers.install}
 
 ${answers.usage}
 
-## Credits 
+## Contributing
 
-${answers.github}
-
-${answers.email}
+${answers.contributing}
 
 ## License
 
-${answers.license}
+${answers.licenses}
 
 ## Tests
 
-${answers.tests}`,
+${answers.tests}
+
+## Questions
+
+For any questions about this project please contact:
+
+github.com/${answers.github}
+
+${answers.email}
+`,
             (err) =>
             err ? console.error(err) : console.log('README file created!'))
         })
